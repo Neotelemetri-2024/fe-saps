a77a5abd8736509672f4ad9b460992ad89a2da2d
shevaramadhan <sheva08.ramadhan@gmail.com>
2026-09-07 17:05:37 +0700

    fix(build): tambahkan pdfkit dan @types/pdfkit ke package.json serta perbaiki typing listener

--- src/services/laporan/pdfLaporan.service.ts
+++ src/services/laporan/pdfLaporan.service.ts
@@ -343,9 +343,9 @@ export async function generatePdfLaporan(data: LaporanDataResult): Promise<Buffe
       })
 
       const chunks: Buffer[] = []
-      doc.on('data', (chunk) => chunks.push(chunk))
+      doc.on('data', (chunk: any) => chunks.push(chunk))
       doc.on('end', () => resolve(Buffer.concat(chunks)))
-      doc.on('error', (err) => reject(err))
+      doc.on('error', (err: any) => reject(err))
 
       registerTimes(doc)
       generatePdf(doc, data)
